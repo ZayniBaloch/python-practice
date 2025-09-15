31efaaabb329c20cf8a1d5782cb9c51785d72300
ZayniBaloch <zaingu89@gmail.com>
2025-09-16 01:00:56 +0500

    added decryption function

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -8,6 +8,16 @@ def encryption(n, plaintext):
         except:
             output+=char
     return output
+# for decryption
+def decryption(n,encrypted_text):
+    output=""
+    for char in encrypted_text:
+        try:
+            i=(key.index(char)-n)%len(key)
+            output+=key[i]
+        except:
+            output+=char
+    return output
 
 # Get input from user
 shift = int(input("Enter shift value (number): "))
@@ -15,8 +25,10 @@ text = input("Enter text to encrypt: ")
 
 # Encrypt the text
 encrypted_text = encryption(shift, text)
+decrypted_text=decryption(shift,encrypted_text)
 
 # Display results
 print(f"Original text: {text}")
 print(f"Encrypted text: {encrypted_text}")
 print(f"Shift value: {shift}")
+print(f"decrypted text: {decrypted_text}")
