f6a36f3db75e5dcdac1d02646e6809d58dc85f22
ZayniBaloch <zaingu89@gmail.com>
2025-09-17 11:05:09 +0500

    added decryption function and solved some encoding problem

--- onetime_pad.py
+++ onetime_pad.py
@@ -9,7 +9,18 @@ def otp_encryption(text):
         key+=chr(num)
     return output,key
     
+    
+def otp_decryption(encrypted_text, key):
+    output = ""
+    for i in range(len(encrypted_text)):
+        key_num = ord(key[i])
+        decrypted_char = chr((ord(encrypted_text[i]) - key_num) % 256)
+        output += decrypted_char
+    return output
+
 text=input("Enter text you want  encrypt :")
+text = text.encode('latin-1').decode('latin-1')
 encrypted_text, encryption_key = otp_encryption(text)
 print("Encrypted text:", encrypted_text)
 print("Key:", encryption_key)
+print(otp_decryption(encrypted_text,encryption_key))
